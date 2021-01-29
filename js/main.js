6450b7b4a757cbe8f41916c35317cca8db13c0e2
ema <elaraji.ilham@gmail.com>
2021-01-29 12:14:24 +0100

    creer les pages pour les differents semstres

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-var pdfmod='';
-var rtr_btn = 0;//button de retour
+var pdfmod = "";
+var rtr_btn = 0; //button de retour
 function nav_a_active(id_a) {
   x = document.getElementById(id_a).parentElement;
-
 }
-window.onscroll = function () { myFunction() };
+window.onscroll = function () {
+  myFunction();
+};
 
 // Get the header
 var header = document.getElementById("myHeader");
@@ -32,41 +33,40 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
-function accedercours(cours,mod_,matiere) {
-  document.getElementById(mod_).style.display = 'inline-grid';
-  document.getElementById(cours).style.display = 'inline-grid';
-  document.getElementById(matiere).style.display = 'none';
-  document.getElementById('rtr-btn').style.display = 'block';
+function accedercours(cours, mod_, matiere) {
+  document.getElementById(mod_).style.display = "inline-grid";
+  document.getElementById(cours).style.display = "inline-grid";
+  document.getElementById(matiere).style.display = "none";
+  document.getElementById("rtr-btn").style.display = "block";
   rtr_btn++;
 }
 
 function voir(pdf, courses, pdfpath) {
-  document.getElementById(pdf).style.display = 'block';
-  document.getElementById(courses).style.display = 'none';
-  document.getElementById('pdfhere').data = pdfpath;
-  pdfmod=courses;
+  document.getElementById(pdf).style.display = "block";
+  document.getElementById(courses).style.display = "none";
+  document.getElementById("pdfhere").data = pdfpath;
+  pdfmod = courses;
 }
 function rtrbtn_(a, b) {
-  document.getElementById(a).style.display = 'inline-grid';
-  document.getElementById(b).style.display = 'none';
+  document.getElementById(a).style.display = "inline-grid";
+  document.getElementById(b).style.display = "none";
   rtr_btn--;
 }
-function retour(id_pluschar = '') {
+function retour(id_pluschar = "") {
   switch (rtr_btn) {
     case 1:
-      dest = 'matiere' + id_pluschar;
-      maintenant = 'lescoursdemodule1' + id_pluschar;
+      dest = "matiere" + id_pluschar;
+      maintenant = "lescoursdemodule1" + id_pluschar;
       rtrbtn_(dest, maintenant);
-      document.getElementById('rtr-btn').style.display = 'none';
+      document.getElementById("rtr-btn").style.display = "none";
       break;
     case 2:
-      dest = 'lescoursdemodule1' + id_pluschar;
-      maintenant = 'courspdf' + id_pluschar;
+      dest = "lescoursdemodule1" + id_pluschar;
+      maintenant = "courspdf" + id_pluschar;
       rtrbtn_(dest, maintenant);
       break;
     default:
       break;
-
   }
 }
 // function searchcours(){
@@ -74,20 +74,26 @@ function retour(id_pluschar = '') {
 //   document.getElementsById('acc-search').style.display='';
 // }
 function inscrire() {
-  document.getElementById('inscrire_form').style.display = 'block';
+  document.getElementById("inscrire_form").style.display = "block";
 }
-function show(id_,scroll_) {
-  document.getElementById(id_).style.display = 'block';
+function show(id_, scroll_) {
+  document.getElementById(id_).style.display = "block";
   window.scrollTo(0, scroll_);
 }
 function hide(id_) {
-  document.getElementById(id_).style.display = 'none';
+  document.getElementById(id_).style.display = "none";
 }
-function precedent(dest,maintenant,displ_y){
+function precedent(dest, maintenant, displ_y) {
   document.getElementById(dest).style.display = displ_y;
-  document.getElementById(maintenant).style.display = 'none';
+  document.getElementById(maintenant).style.display = "none";
 }
-function precedentpdf(maintenant,displ_y){
+function precedentpdf(maintenant, displ_y) {
   document.getElementById(pdfmod).style.display = displ_y;
-  document.getElementById(maintenant).style.display = 'none';
+  document.getElementById(maintenant).style.display = "none";
+}
+
+function semstrelink(id_, id_1, id_2) {
+  document.getElementById(id_).style.display = "block";
+  document.getElementById(id_1).style.display = "none";
+  document.getElementById(id_2).style.display = "none";
 }
