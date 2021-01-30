3a1e0882ed4ab4deda4ffd34e7782d83080a5677
ema <elaraji.ilham@gmail.com>
2021-01-30 15:21:15 +0100

    ajouter les media queries pour mode tablet

--- js/main.js
+++ js/main.js
@@ -25,7 +25,13 @@ function myFunction() {
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
+  if (screen.width<=630) {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("mySidebar").style.Left = "0px";
+
+  }
   document.getElementById("main").style.marginLeft = "250px";
+
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
